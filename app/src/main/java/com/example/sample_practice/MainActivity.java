caca048579861f35adc37755d74d3dd9a8ad9352
Transformer075 <haseebiftikhar452@gmail.com>
2021-11-05 18:41:01 +0500

    Switch & Checbox added

--- app/src/main/java/com/example/sample_practice/MainActivity.java
+++ app/src/main/java/com/example/sample_practice/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     Button btn1;
     TextView textView1;
     TextView textView2;
+    CheckBox checkBox1;
+    Switch switch1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +24,27 @@ public class MainActivity extends AppCompatActivity {
         textView1 = findViewById(R.id.name);
         textView2 = findViewById(R.id.result);
         btn1 = findViewById(R.id.btn_1);
+        checkBox1 = findViewById(R.id.checkbox1);
+        switch1 = findViewById(R.id.switch1);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textView2.setText(textView1.getText().toString());
+                if(checkBox1.isChecked()) {
+                    textView2.setText(textView1.getText().toString());
+                }
+                else
+                    textView2.setText("Checkbox not checked");
             }
         });
 
     }
-
-
-
-
+    public void switchClicked(View view){
+        if(switch1.isChecked()) {
+            switch1.setText("On");
+        }
+        else
+            switch1.setText("Off");
+    }
 
 }
