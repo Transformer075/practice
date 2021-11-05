31b7403fb49e2018166a030b33073d54c704912e
Transformer075 <haseebiftikhar452@gmail.com>
2021-11-06 03:28:44 +0500

    Implicit Dial_Intent Added

--- app/src/main/java/com/example/sample_practice/MainActivity2.java
+++ app/src/main/java/com/example/sample_practice/MainActivity2.java
@@ -2,20 +2,44 @@ package com.example.sample_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    TextView textView;
+    TextView textView1;
+    TextView textView2;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        textView = findViewById(R.id.txtview1);
+        textView1 = findViewById(R.id.txtview1);
         Intent intent = getIntent();
-        textView.setText(intent.getStringExtra("key"));
+        textView1.setText(intent.getStringExtra("key"));
+
+        textView2 = findViewById(R.id.editTextPhone);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("tel:+923001234567");
+
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+
+                startActivity(intent);
+
+
+            }
+        });
+
     }
 }
