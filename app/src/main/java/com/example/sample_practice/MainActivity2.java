8ee9dd6a4a0e99d1a2120e0fe1e1444a505ede60
Transformer075 <haseebiftikhar452@gmail.com>
2021-11-16 23:25:34 +0500

    Toast Added

--- app/src/main/java/com/example/sample_practice/MainActivity2.java
+++ app/src/main/java/com/example/sample_practice/MainActivity2.java
@@ -10,12 +10,15 @@ import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
     TextView textView1;
     TextView textView2;
     Button button;
+    Button toastButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +30,7 @@ public class MainActivity2 extends AppCompatActivity {
 
         textView2 = findViewById(R.id.editTextPhone);
         button = findViewById(R.id.button);
+        toastButton = findViewById(R.id.button3);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +42,15 @@ public class MainActivity2 extends AppCompatActivity {
                 startActivity(intent);
 
 
+            }
+        });
+
+        Toast toast = Toast.makeText(this, "Hello, I am Haseeb", Toast.LENGTH_LONG);
+
+        toastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toast.show();
             }
         });
 
